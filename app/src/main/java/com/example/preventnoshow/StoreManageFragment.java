a955c3eaa68e68aa28bca2f0714a27afd18bfbf2
simja <simjayeon98@gmail.com>
2020-12-02 02:43:26 +0900

    3

--- app/src/main/java/com/example/preventnoshow/StoreManageFragment.java
+++ app/src/main/java/com/example/preventnoshow/StoreManageFragment.java
@@ -8,11 +8,32 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ListView;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import static com.example.preventnoshow.RemoteService.BASE_URL2;
+import static com.example.preventnoshow.RemoteService.BASE_URL3;
 
 public class StoreManageFragment extends Fragment {
     FloatingActionButton btnAdd;
+    Retrofit retrofit;
+    RemoteService remoteService;
+    ListView listSM;
+    SMAdapter smAdapter = new SMAdapter();
+    List<StoreVO> smList = new ArrayList<>();
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -28,7 +49,58 @@ public class StoreManageFragment extends Fragment {
            }
        });
 
+       listSM = view.findViewById(R.id.blistStore);
+       listSM.setAdapter(smAdapter);
 
+        Gson gson = new GsonBuilder()
+                .setLenient()
+                .create();
+
+        retrofit = new Retrofit.Builder()
+                .baseUrl(BASE_URL2)
+                .addConverterFactory(GsonConverterFactory.create(gson))
+                .build();
+        remoteService = retrofit.create(RemoteService.class);
+
+        Call<List<StoreVO>> call = remoteService.listStore();
+        call.enqueue(new Callback<List<StoreVO>>() {
+            @Override
+            public void onResponse(Call<List<StoreVO>> call, Response<List<StoreVO>> response) {
+                smList = response.body();
+                smAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onFailure(Call<List<StoreVO>> call, Throwable t) {
+
+            }
+        });
         return view;
+    }
+
+    class SMAdapter extends BaseAdapter{
+
+        @Override
+        public int getCount() {
+            return smList.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View view, ViewGroup parent) {
+           // view = getActivity().getLayoutInflater().inflate(R.layout.item_boss, parent, false);
+            //여기 수정해야함
+
+            return null;
+        }
     }
 }
